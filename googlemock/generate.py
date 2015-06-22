1e9292ae592e7ba9fc5d87381589063807ccb1c6
Ivan Safonov <safonov.ivan.s@gmail.com>
2015-06-22 14:47:11 +0600

    Faster build

--- googlemock/generate.py
+++ googlemock/generate.py
@@ -10,16 +10,21 @@ parser.add_option("-c", "--class-name", dest="classname")
 parser.add_option("-t", "--target-path", dest="targetpath")
 parser.add_option("-s", "--source-object", dest="sourceobject")
 parser.add_option("-e", "--header-suffix", dest="headersuffix")
+parser.add_option("-E", "--source-suffix", dest="sourcesuffix")
 parser.add_option("-v", "--only-virtual", dest="onlyvirtual", default="true")
 parser.add_option("-p", "--public", dest="public", default="true")
 parser.add_option("-r", "--protected", dest="protected", default="false")
 parser.add_option("-i", "--private", dest="private", default="false")
+parser.add_option("-S", "--faster-build", dest="fasterbuild", default="false")
 (options, args) = parser.parse_args()
 
-newFileName = str(options.classname).lower() + '.' + options.headersuffix
+newFileNameH = str(options.classname).lower() + '.' + options.headersuffix
+newFileNameCpp = str(options.classname).lower() + '.' + options.sourcesuffix
 
 if options.dryrun:
-    print newFileName + ',openeditor\n'
+    print newFileNameH + ',openeditor\n'
+    if options.fasterbuild:
+        print newFileNameCpp + ',openeditor\n'
     exit(0)
 
 accessors = []
@@ -36,23 +41,25 @@ except CppHeaderParser.CppParseError as e:
     print(e)
     sys.exit(1)
 
-newFilePath = options.targetpath + '/' + newFileName
-newFile = open(newFilePath, 'w')
-newFile.write('#ifndef %s_H\n' % str(options.classname).upper())
-newFile.write('#define %s_H\n' % str(options.classname).upper())
-
-newFile.write('\n#include "gmock/gmock.h"\n')
-newFile.write('#include "%s"\n' % os.path.basename(options.sourceobject))
+newFilePathH = options.targetpath + '/' + newFileNameH
+newFileH = open(newFilePathH, 'w')
+newFileH.write('#ifndef %s_H\n' % str(options.classname).upper())
+newFileH.write('#define %s_H\n' % str(options.classname).upper())
+newFileH.write('\n#include "gmock/gmock.h"\n')
+newFileH.write('#include "%s"\n' % os.path.basename(options.sourceobject))
 
 for className in cppHeader.classes:
     if '::' in className:
         continue
-    newFile.write('\n/**\n');
-    newFile.write(' * @brief Automatically generated Mock object for %s\n' % className);
-    newFile.write(' */\n');
-    newFile.write('class %s : public %s\n' %(options.classname, className))
-    newFile.write('{\n')
-    newFile.write('public:\n')
+    newFileH.write('\n/**\n');
+    newFileH.write(' * @brief Automatically generated Mock object for %s\n' % className);
+    newFileH.write(' */\n');
+    newFileH.write('class %s : public %s\n' %(options.classname, className))
+    newFileH.write('{\n')
+    newFileH.write('public:\n')
+    if options.fasterbuild:
+        newFileH.write('    %s();\n' % options.classname)
+        newFileH.write('    virtual ~%s();\n\n' % options.classname)
     
     classObj = cppHeader.classes[className]
     for accessor in accessors:
@@ -69,9 +76,17 @@ for className in cppHeader.classes:
             params = []
             for parameter in method['parameters']:
                 params.append('%s %s' % (parameter['type'], parameter['name']))
-            newFile.write('    %s%s(%s, %s(%s));\n' %(mockMethod, paramsCount, methodName, rtnType, ', '.join(params)))
+            newFileH.write('    %s%s(%s, %s(%s));\n' %(mockMethod, paramsCount, methodName, rtnType, ', '.join(params)))
 
-    newFile.write('};\n')
+    newFileH.write('};\n')
 
-newFile.write('\n#endif // %s_H\n' % str(options.classname).upper())
-newFile.close()
+newFileH.write('\n#endif // %s_H\n' % str(options.classname).upper())
+newFileH.close()
+
+if options.fasterbuild:
+    newFilePathCpp = options.targetpath + '/' + newFileNameCpp
+    newFileCpp = open(newFilePathCpp, 'w')
+    newFileCpp.write('#include "%s"\n\n' % newFileNameH)
+    newFileCpp.write('%s::%s()\n{\n}\n\n' % (options.classname, options.classname))
+    newFileCpp.write('%s::~%s()\n{\n}\n' % (options.classname, options.classname))
+    newFileCpp.close()
