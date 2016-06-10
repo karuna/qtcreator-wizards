f7516656945306d47e8ccb97faae825e2167649e
Karuna Murti <karuna.murti@gmail.com>
2016-06-10 19:23:14 +0900

    follow template reference

--- googletest/file.cpp
+++ googletest/file.cpp
@@ -1,39 +1,59 @@
 %{Cpp:LicenseTemplate}\
+#include "this/package/foo.h"
 #include "gtest/gtest.h"
 
-/**
- * @brief Automatically generated Google Test skeleton
- */
-class %{TestName} : public ::testing::Test
-{
-protected:
+namespace {
+  // The fixture for testing class Foo.
+  class %{TestName} : public ::testing::Test {
+  protected:
+    // You can remove any or all of the following functions if its body
+    // is empty.
+
     %{TestName}() {
-        // You can do set-up work for each test here.
+      // You can do set-up work for each test here.
     }
 
     virtual ~%{TestName}() {
-        // You can do clean-up work that doesn't throw exceptions here.
+      // You can do clean-up work that doesn't throw exceptions here.
     }
 
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
     void SetUp() {
-        // Code here will be called immediately after the constructor (right
-        // before each test).
+      // Code here will be called immediately after the constructor (right
+      // before each test).
     }
 
-    void TearDown() {
-        // Code here will be called immediately after each test (right
-        // before the destructor).
+    virtual void TearDown() {
+      // Code here will be called immediately after each test (right
+      // before the destructor).
     }
 
     static void SetUpTestCase() {
-        // Called before the first test in this test case.
+      // Called before the first test in this test case.
     }
 
     static void TearDownTestCase() {
-        // Called after the last test in this test case.
+      // Called after the last test in this test case.
     }
-};
+    // Objects declared here can be used by all tests in the test case for Foo.
+  };
 
-TEST_F(%{TestName}, FirstTest) {
-    // Test
+  // Tests that the Foo::Bar() method does Abc.
+  TEST_F(%{TestName}, MethodBarDoesAbc) {
+    const string input_filepath = "this/package/testdata/myinputfile.dat";
+    const string output_filepath = "this/package/testdata/myoutputfile.dat";
+    Foo f;
+    EXPECT_EQ(0, f.Bar(input_filepath, output_filepath));
+  }
+
+  // Tests that Foo does Xyz.
+  TEST_F(%{TestName}, DoesXyz) {
+    // Exercises the Xyz feature of Foo.
+  }
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
